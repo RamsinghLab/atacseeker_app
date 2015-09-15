664acf61d31f6672643336206678660f835ae637
Asif Zubair <asif.zubair@gmail.com>
2015-09-14 21:15:24 -0700

    Create callbacks.js

---
+++ nativeApp/callbacks.js
@@ -0,0 +1,16 @@
+function launchSpec(dataProvider)
+ {
+  
+       
+     
+   var retval = {
+ 
+            commandLine: ["",""],
+            containerImageId: "asifzubair/atacseq_counts:v44",
+            options:["bsfs.enabled=true"]
+           
+     
+           };
+    return retval;  
+        
+}
