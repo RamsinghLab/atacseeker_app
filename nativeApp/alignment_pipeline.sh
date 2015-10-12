1dab0410cd78f60ba81f0329d5cf4cb7fb0a739b
asifzubair <asif.zubair@gmail.com>
2015-10-11 22:44:07 -0700

    close to final alignment pipeline

--- nativeApp/alignment_pipeline.sh
+++ nativeApp/alignment_pipeline.sh
@@ -1,34 +1,17 @@
 #!/bin/bash -x
 
-DATA="/atacseq_counts/data"
+DATA=$1
 
 SUBJECT="normal1"
 SUFFIX="1mReads.fastq"
 FASTQS=$SUBJECT"_1."$SUFFIX" "$SUBJECT"_2."$SUFFIX 
 
-## echo $FASTQS
-## > normal1_1.1mReads.fastq normal1_2.1mReads.fastq
 
 REF_PATH="/atacseeker/reference/" 
-GENOME="chr22" ## let's hope we know this in advance!!
+GENOME="hg19" ## let's hope we know this in advance!!
 CHRSIZES=$GENOME".chrom.sizes" ## assuming we do
 
-## check quality of reads in fastq files
-## 
-## > good idea! but you can do this separately in BaseSpace with its own app --t
-## > if we were going to do it locally, a loop would make life a little easier:
-## 
-## for FASTQ in $FASTQS; do
-##   fastqc $FASTQ
-## done
-
-## index the reference
 ##
-## > also a good idea, but should be done beforehand & uploaded with the app --t
-##
-## bwa index $GENOME.fa
-
-
 ## align paired end reads by using bwa mem,
 ## pipe it to samblaster to remove duplicates, 
 ## and use samtools to make BAM file
@@ -54,6 +37,7 @@ bwa mem -M $REF_PATH$GENOME.fa $FASTQS | \
   grep -v "chrM" | \
   samtools view $FLAGS - > $OUTBAM
 
+##
 ## sort and index the BAM for downstream processing
 ##
 
@@ -61,6 +45,3 @@ SORTED=`basename $OUTBAM .bam`.sorted
 samtools sort $OUTBAM $SORTED 
 samtools index $SORTED".bam"
 
-## if you wanted to, you could bamToBigWig it at this point:
-##
-#./bamToBigWig $SORTED $GENOME
