728278ecd36a1aa0877b2ec92a0fc6b26e05a290
Asif Zubair <asif.zubair@gmail.com>
2015-10-16 12:54:41 -0700

    Update install_samblaster.sh

--- nativeApp/tmp/scripts/install_samblaster.sh
+++ nativeApp/tmp/scripts/install_samblaster.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# This script will download samblaster and install it
-# It then copies samblaster to the local/bin so that it can be called easily
+####
+## This script will download samblaster and install it
+## It then copies samblaster to the local/bin so that it can be called easily
+####
 
 cd /atacseeker/install
 git clone git://github.com/GregoryFaust/samblaster.git
