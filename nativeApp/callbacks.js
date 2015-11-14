c42b078de2c773fa25c16d5bbfb2559ed93f6b7d
asifzubair <asif.zubair@gmail.com>
2015-11-14 15:08:46 -0800

    fixed quotes

--- nativeApp/callbacks.js
+++ nativeApp/callbacks.js
@@ -2,7 +2,7 @@ function launchSpec(dataProvider){
 
    var retval = {
  
-            commandLine: ["Rscript","-e 'rmarkdown::render("/atacseeker/scripts/atacseeker.Rmd")'"],
+            commandLine: ["Rscript","-e 'rmarkdown::render('/atacseeker/scripts/atacseeker.Rmd')'"],
             containerImageId: "asifzubair/atacseeker:v1",
             options:["bsfs.enabled=true"]
             
