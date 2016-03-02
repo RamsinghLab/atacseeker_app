43fa6283d6df05801c7dce93b9ff1183132b1fef
Asif Zubair <asif.zubair@gmail.com>
2016-03-02 13:10:21 -0800

    Update install_packages.R

--- nativeApp/tmp/scripts/install_packages.R
+++ nativeApp/tmp/scripts/install_packages.R
@@ -13,18 +13,8 @@ rm(r)
 ##
 
 ## CRAN packages
-## install.packages('knitr')
-## install.packages('rmarkdown')
-## install.packages('rjson')
-## install.packages('statmod')
-## install.packages('locfit')
-
 install.packages(c("knitr", "rmarkdown", "rjson", "statmod", "locfit"))
 
 ## Bioconductor packages
 source("https://bioconductor.org/biocLite.R")
-## biocLite('edgeR')
-## biocLite('csaw')
-## biocLite('Homo.sapiens')
-## biocLite('Gviz')
 biocLite(pkgs=c("Rsamtools", "rtracklayer", "edgeR", "csaw", "Homo.sapiens", "LOLA", "PWMEnrich", "PWMEnrich.Hsapiens.background", "BSgenome.Hsapiens.UCSC.hg19"))
