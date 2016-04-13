88ce4ee9d4c19367a8977220cdb2158b6142278a
asifzubair <asif.zubair@gmail.com>
2016-04-12 18:57:32 -0700

    oh man

--- mtoolbox/chr.M.100214/run_mtoolbox.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-HOME="/home/cmb-07/sn1/asifzuba"
-
-MToolBox.sh -i bam -r RCRS \
--m "-g ${HOME}/software_frozen/local/bin/gsnap -D ${HOME}/software_frozen/local/share/gmapdb/ -M chrM -H hg19RCRS " \
--a "-r ${HOME}/software_frozen/local/share/genomes/ -f chrM.fa -a hg19RCRS.fa -s ${HOME}/software_frozen/src/anaconda/envs/mtoolbox/bin/samtools " \
--c "-m ${HOME}/software_frozen/src/anaconda/envs/mtoolbox/bin/muscle" \
--s ${HOME}/software_frozen/src/anaconda/envs/mtoolbox/bin/samtools
