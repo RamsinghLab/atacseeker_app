42cde4cd0c3baa28b68866931940296622540ee6
asifzubair <asif.zubair@gmail.com>
2016-05-07 15:01:45 -0700

    atacseeker

--- nativeApp/tmp/scripts/atacseeker.R
+++ nativeApp/tmp/scripts/atacseeker.R
@@ -33,27 +33,25 @@ qcPlot <- function(frag.dists, label) {
 }
 
 
-
-## plot the top differentially accessible regions using Gviz
 ## plot the top differentially accessible regions using Gviz
 plotRegion <- function(region) { 
     olReads <- lapply(bam.files, extractReads, region=region, param=discard.se.param)
     for (x in seq(num_samples)) 
         metadata(olReads[[x]])$totals <- colData(data[,x])$totals
     covs <- GRangesList(lapply(olReads, function(x) 
-        as(coverage(x) / metadata(x)$totals, "GRanges")))
+        as(coverage(x) *1e6/ metadata(x)$totals, "GRanges")))
     maxDepth <- max(sapply(covs, function(x) max(score(x))))
     cols <- c("darkred","darkgreen")[seq_along(bam.files) %% 2 + 1]
     names(cols) <- samples
     collected <- list()
-    for(i in bam.files) {
+    for(i in seq(num_samples)) {
         covr <- covs[[i]]
-        ctrack <- DataTrack(covr, type="histogram", lwd=0, fill=cols[i], name=i, 
+        ctrack <- DataTrack(covr, type="histogram", lwd=0, fill=cols[i], name=samples[i], 
                             ylim=c(0,maxDepth), col.axis="black", col.title="black")
         collected[[i]] <- OverlayTrack(trackList=list(coverage=ctrack))
     }
     gax <- GenomeAxisTrack(col="black")
-    plotTracks(c(gax, collected), from=start(region), to=end(region))
+    plotTracks(c(gax, collected), from=start(region), to=end(region), chromosome=as.character(seqnames(region)))
     invisible(covs)
 }
 
