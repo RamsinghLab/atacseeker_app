6b1c9867895353f77b03e3a3debe666a02d5f204
Asif Zubair <asif.zubair@gmail.com>
2016-06-10 18:07:06 -0700

    Update assembleMTgenome.py

--- atacseeker/scripts/assembleMTgenome.py
+++ atacseeker/scripts/assembleMTgenome.py
@@ -424,8 +424,8 @@ mt_table_handle = tablefile
 sam_file = open(basext+'.sam', 'r')
 mt_table = open(tablefile, 'r').readlines()
 mut_events = mtvcf_main_analysis(mt_table, sam_file, sample_name, tail=tail)
-if os.path.exists('../VCF_dict_tmp'):
-	VCF_dict = ast.literal_eval(open('../VCF_dict_tmp', 'r').read()) # global VCF dict
+if os.path.exists('VCF_dict_tmp'):
+	VCF_dict = ast.literal_eval(open('VCF_dict_tmp', 'r').read()) # global VCF dict
 else:
 	VCF_dict = {} # global VCF dict
 contigs_wdict = []
@@ -487,7 +487,7 @@ if crf: f.close()
 
 if mut_events:
     VCF_dict.update(mut_events)
-mut_events_cellar = open('../VCF_dict_tmp', 'w')
+mut_events_cellar = open('VCF_dict_tmp', 'w')
 mut_events_cellar.write(str(VCF_dict))
 mut_events_cellar.close()
 
