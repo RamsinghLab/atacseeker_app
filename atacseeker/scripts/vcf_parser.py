74e482e045cae8cd06eed3104ed4ac8138a316ce
asif zubair <asifzuba@usc.edu>
2016-06-25 12:41:34 -0700

    het_plasmy

--- atacseeker/scripts/vcf_parser.py
+++ atacseeker/scripts/vcf_parser.py
@@ -3,21 +3,21 @@ import vcf
 def print_alleles(data, sep = "\t"):
     num_alleles = len(data.GT.split("/"))
     if  num_alleles == 1:
-        print sep.join([str(x) for x in [0., 0, 0., 0, 0.]]),
+        print sep.join([str(x) for x in ["-", 0., "-", 0.]]),
     elif num_alleles == 2:
-        print sep.join([str(x) for x in [data.DP, rec.ALT[0], data.HF, 0, 0.]]),
+        print sep.join([str(x) for x in [rec.ALT[0], data.HF, "-", 0.]]),
     elif num_alleles == 3:
-        print sep.join([str(x) for x in [data.DP, rec.ALT[0], data.HF[0], rec.ALT[1], data.HF[1]]]),
+        print sep.join([str(x) for x in [rec.ALT[0], data.HF[0], rec.ALT[1], data.HF[1]]]),
 
 reader = vcf.Reader(open('VCF_file.vcf.gz', 'r'))
 sep = "\t"
-print sep.join(["CHROM", "POS", "REF"]), sep,
-print sep.join([reader.samples[0] + "-" + i  for i in ["DEPTH", "ALT1", "VAF1", "ALT2", "VAF2"]]), sep,
-print sep.join([reader.samples[1] + "-" + i  for i in ["DEPTH", "ALT1", "VAF1", "ALT2", "VAF2"]]),
-print
+print "REF", sep,
+print sep.join(["S-" + i  for i in ["ALT1", "VAF1", "ALT2", "VAF2"]]), sep,
+print sep.join(["N-" + i  for i in ["ALT1", "VAF1", "ALT2", "VAF2"]]), sep,
+print sep.join(["CHROM", "POS"])
 for rec in reader:
-    print rec.CHROM, sep, rec.POS, sep, rec.REF, sep,
+    print rec.REF, sep,
     print_alleles(rec.samples[0].data, sep)
     print sep,    
     print_alleles(rec.samples[1].data, sep)
-    print
+    print sep, rec.CHROM, sep, rec.POS
