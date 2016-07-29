607e6cccfda8d8ead9e630c1db3552d0213af5f4
Asif Zubair <asif.zubair@gmail.com>
2016-07-28 20:25:50 -0700

    Update assembleMTgenome.sh

--- atacseeker/scripts/assembleMTgenome.sh
+++ atacseeker/scripts/assembleMTgenome.sh
@@ -16,7 +16,7 @@ samtools sort ${chrM_bam} ${dir}/${in_base}.sorted
 samtools rmdup ${dir}/${in_base}.sorted.bam ${dir}/${in_base}.sorted.nodup.bam
 samtools index ${dir}/${in_base}.sorted.nodup.bam
 
-python assembleMTgenome.py \
+python ${BASE}/scripts/assembleMTgenome.py \
 -f ${BASE}/reference/chrM.fa \
 -i ${dir}/${in_base}.sorted.nodup.bam \
 -a ${BASE}/reference/hg19.fa \
