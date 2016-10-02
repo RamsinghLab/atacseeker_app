02724335b3985b27d4c3f969e26adb54aeac686e
Asif Zubair <asif.zubair@gmail.com>
2016-10-01 23:21:13 -0700

    Update install_git_packages.sh

--- atacseeker/scripts/install_git_packages.sh
+++ atacseeker/scripts/install_git_packages.sh
@@ -6,6 +6,8 @@ git clone --recursive git://github.com/ekg/freebayes.git
 cd freebayes
 make
 make install
+cd /tmp
+rm -rf freebayes
 
 ## vcflib
 cd /tmp
@@ -13,6 +15,8 @@ git clone --recursive https://github.com/vcflib/vcflib.git
 cd vcflib
 make
 cp bin/vcffilter /usr/local/bin
+cd /tmp
+rm -rf vcflib
 
 ## bamaddrg
 cd /tmp
@@ -20,3 +24,5 @@ git clone --recursive https://github.com/ekg/bamaddrg
 cd bamaddrg
 make
 cp bamaddrg /usr/local/bin
+cd /tmp
+rm -rf bamaddrg
